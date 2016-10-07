0d0197e1012187781dd44507a0a3259d8ca23558
hinatades <hnttisk@gmail.com>
2016-10-07 10:15:36 +0900

    add company-mode

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 (require 'cask)
 (cask-initialize)
 
+;; setting company
+(require 'company)
+(global-company-mode) ; 全バッファで有効にする 
+(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-minimum-prefix-length 2) ; デフォルトは4
+(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+
 ;; theme
 (load-theme 'atom-one-dark t)
 
