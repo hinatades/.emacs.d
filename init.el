b960c11d66bc04ba650896ab2788585e579bd4c7
hinatades <hnttisk@gmail.com>
2018-01-12 02:37:26 +0900

    Enabled auto-complete in rjsx-mode

--- init.el
+++ init.el
@@ -248,6 +248,22 @@
 ;; rjsx-mode
 (require 'rjsx-mode)
 (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("containers\\/.*\\.js\\'" . rjsx-mode))
+
+(add-hook 'rjsx-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil) ;;インデントはタブではなくスペース
+            (setq js-indent-level 2) ;;スペースは２つ、デフォルトは4
+            (setq js2-strict-missing-semi-warning nil))) ;;行末のセミコロンの警告はオフ
+
+;; rjsx-mode時にauto-completeを有効可
+(add-hook 'rjsx-mode-hook '(lambda ()
+                                   (require 'auto-complete)
+                                   (auto-complete-mode t)
+                                   ))
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'rjsx-mode)
 
 
 ;; markdown-mode
